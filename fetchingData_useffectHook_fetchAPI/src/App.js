51356218b0edcbec87354ed1d11b69a70887445b
Caglar <ahmetcmirza@gmail.com>
2021-11-07 14:35:40 +0100

    fetch and async await

--- fetchingData_useffectHook_fetchAPI/src/App.js
+++ fetchingData_useffectHook_fetchAPI/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, {useState , useEffect} from "react";
 import './App.css';
 
-function App() {
+function App(props) {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() =>{
+
+
+  // fetch('https://jsonplaceholder.typicode.com/posts')
+  // .then((response) => response.json())
+  // .then((data) => setData(data.splice(0,10)))
+
+  (async ()=>{
+    const responce = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await responce.json();
+    setTimeout(() => {
+      setData(data.splice(0,10));
+      setIsLoading(false)
+      
+    }, 3000);
+
+  })();
+  
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {isLoading ? <div>Loading</div> :
+      <ul>
+        {data.map((item)=>{
+          return <li key = {item.id}>{item.body}</li>
+        })}
+      </ul>
+      }
+      </div>
+      
+      
     </div>
   );
 }
